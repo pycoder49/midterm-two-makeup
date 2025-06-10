f4b1a0088b1c87d9ec1cf6be447a3cc8c7773123
Aryan Ahuja <75223447+pycoder49@users.noreply.github.com>
2025-06-09 21:51:32 -0700

    Update script.js

--- refactored_code/script.js
+++ refactored_code/script.js
@@ -1,36 +1,43 @@
-// Written by mlazze@globocorp.com - email me at mlazze1337dev@gmail.com for freelance work!
-var messages = [];
-var count = 0;
+document.addEventListener('DOMContentLoaded', () => {
+    var messages = [];
+    var count = 0;
 
-function send() {
-  var input = document.getElementById('input');
-  var prompt = input.value;
+    var chat = document.getElementById('chat');
+    var input = document.getElementById('input');
+    var button = document.querySelector('button');
 
-  if (prompt == '') {
-    alert('Type something!');
-    return;
-  }
+    function send() {
+        var prompt = input.value;
+        if (prompt === '') {
+            alert('Type something!');
+            return;
+        }
 
-  count++;
-  messages.push(prompt);
+        count++;
+        messages.push(prompt);
 
-  var chat = document.getElementById('chat');
-  chat.innerHTML += '<div class="message user">You: ' + prompt + '</div>';
-  input.value = '';
+        chat.innerHTML += '<div class="message user">' + prompt + '</div>';
+        input.value = '';
 
-  // TODO: Simulate the chat for now
-  // replace with a to askGPTGenie(prompt)
+        setTimeout(function () {
+            var replies = ["Cool!", "Awesome!", "Noice", "Killer idea", "Lowkey fire", "Fire", "Goated idea", "I hear you...", "Sounds like a plan"];
+            var reply = replies[Math.floor(Math.random() * replies.length)];
+            chat.innerHTML += '<div class="message bot">GENIE<br>' + reply + '</div>';
+            chat.scrollTop = chat.scrollHeight;
+        }, 500);
+    }
 
-  setTimeout(function() {
-    var replies = ["Cool!", "Awesome!", "Noice", "Killer idea", "Lowkey fire", "Fire", "Goated idea", "I hear you...", "Sounds like a plan"];
-    var reply = replies[Math.floor(Math.random() * replies.length)];
-    chat.innerHTML += '<div class="message bot">Bot: ' + reply + '</div>';
-    chat.scrollTop = chat.scrollHeight;
-  }, 500);
-  // End Simulate
+    input.addEventListener('keypress', (event) => {
+        if (event.keyCode === 13) {
+            send();
+        }
+    });
 
-}
+    button.addEventListener('click', () => {
+        send();
+    });
 
-var appVersion = "1.0";
-var appName = "GloboCorp's GPT Genie";
-console.log(appName + " " + appVersion + " loaded @ " + new Date());
+    var appVersion = "1.0";
+    var appName = "GloboCorp's GPT Genie";
+    console.log(appName + " " + appVersion + " loaded @ " + new Date());
+});
